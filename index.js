4b47939cfa489a3e46f889184a3e757cc7370433
Kusi Musah Hussein <kusimusah@gmail.com>
2020-02-12 15:50:11 +0300

    safe commit

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const jsonToXlsx = require('./json-to-xlsx');
 const xlsxToJson = require('./xlsx-to-json');
 
-const jsDoc = require('jsdoc-to-markdown');
-
-jsDoc.render({files: 'index.js'}).then(data => console.log(data))
-
 module.exports = {
     jsonToXlsx,
     xlsxToJson
