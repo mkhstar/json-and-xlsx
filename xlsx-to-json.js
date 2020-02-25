08ab5058fe345d8b6afc759224196af3fc8c3ef4
Kusi Musah Hussein <kusimusah@gmail.com>
2020-02-25 11:06:17 +0300

    added default dot

--- xlsx-to-json.js
+++ xlsx-to-json.js
@@ -1,82 +1,86 @@
-const {writeFile} = require('./helpers/promisified-funcs');
+const { writeFile } = require('./helpers/promisified-funcs');
 
-const dot = require('dot-object');
+const Dot = require('dot-object');
 const xlsx = require('xlsx');
 
 async function readToFile(xlsxData, filePath = 'output.json') {
-   const jsonData = renderJson(xlsxData);
+  const jsonData = renderJson(xlsxData);
 
-    await writeFile(filePath, JSON.stringify(jsonData, null, 2));
-    return true;
+  await writeFile(filePath, JSON.stringify(jsonData, null, 2));
+  return true;
 }
 
 /**
- * 
+ *
  * @param {workbook} xlsxData The workbook to get in js object
  */
 
-function readAndGet(xlsxData) {
-    const jsonData = renderJson(xlsxData);
-    return jsonData;
+function readAndGet(xlsxData, defaultDot = '.') {
+  const jsonData = renderJson(xlsxData, defaultDot);
+  return jsonData;
 }
 
 /**
- * 
+ *
  * @param {Buffer} buffer The buffer to get in js object
  */
 
-function readFromBufferAndGet(buffer) {
-   const workbook = xlsx.read(buffer);
-   const jsonData = renderJson(workbook);
-   return jsonData;
+function readFromBufferAndGet(buffer, defaultDot = '.') {
+  const workbook = xlsx.read(buffer);
+  const jsonData = renderJson(workbook, defaultDot);
+  return jsonData;
 }
 
-
 /**
- * 
+ *
  * @param {string} sourceFilePath The workbook to get in js
  */
 
-function readFromFileAndGet(sourceFilePath) {
-    const xlsxData = xlsx.readFile(sourceFilePath);
-    const jsonData = renderJson(xlsxData);
+function readFromFileAndGet(sourceFilePath, defaultDot = '.') {
+  const xlsxData = xlsx.readFile(sourceFilePath);
+  const jsonData = renderJson(xlsxData, defaultDot);
 
-    return jsonData;
+  return jsonData;
 }
 
 /**
- * 
+ *
  * @param {string} sourceFilePath The soruce file path of the workbook
  * @param {string} destFilePath The output json file path
  */
 
-async function readFromFileToFile(sourceFilePath, destFilePath = 'output.json') {
-    const xlsxData = xlsx.readFile(sourceFilePath);
-    const jsonData = renderJson(xlsxData);
+async function readFromFileToFile(
+  sourceFilePath,
+  destFilePath = 'output.json',
+  defaultDot = '.'
+) {
+  const xlsxData = xlsx.readFile(sourceFilePath);
+  const jsonData = renderJson(xlsxData, defaultDot);
 
-    await writeFile(destFilePath, JSON.stringify(jsonData, null, 2));
-    return true;
+  await writeFile(destFilePath, JSON.stringify(jsonData, null, 2));
+  return true;
 }
 
 /**
- * 
+ *
  * @param {workbook} xlsxData The workbook to get in js
  */
-function renderJson(xlsxData) {
-    if (!(xlsxData && xlsxData.Sheets)) return console.error('Please pass a valid workbook');
+function renderJson(xlsxData, defaultDot = '.') {
+  if (!(xlsxData && xlsxData.Sheets))
+    return console.error('Please pass a valid workbook');
 
-    const sheetName = xlsxData.SheetNames[0];
-    let jsonData = xlsx.utils.sheet_to_json(xlsxData.Sheets[sheetName]);
+  const sheetName = xlsxData.SheetNames[0];
+  let jsonData = xlsx.utils.sheet_to_json(xlsxData.Sheets[sheetName]);
 
-    jsonData.map(jd => dot.object(jd));
-    return jsonData;
+  const dot = new Dot(defaultDot);
+  jsonData.map(jd => dot.object(jd));
+  return jsonData;
 }
-
 
 module.exports = {
-    readToFile,
-    readAndGet,
-    readFromBufferAndGet,
-    readFromFileAndGet,
-    readFromFileToFile
-}
+  readToFile,
+  readAndGet,
+  readFromBufferAndGet,
+  readFromFileAndGet,
+  readFromFileToFile
+};
